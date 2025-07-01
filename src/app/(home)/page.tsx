b85eb7a902558483cb17649b794be19c515d3c1b
Nikhil Prajapati <nikhilprajapati3003@gmail.com>
2025-07-01 18:14:52 +0530

    add categories carousel

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,27 @@
+// "use client";
+
+import { HomeView } from "@/modules/home/ui/views/home-view";
+import { HydrateClient, trpc } from "@/trpc/server";
+
+
+export const dynamic = "force-dynamic";
+
+interface PageProps {
+  searchParams: Promise<{
+    categoryId?: string;
+  }>;
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  const { categoryId } = await searchParams;
+  void trpc.categories.getMany.prefetch();
+  return (
+    <div>
+      <HydrateClient>
+        <HomeView categoryId={categoryId} />
+      </HydrateClient>
+    </div>
+  );
+};
+
+export default Page;
