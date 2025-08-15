a2702c09800ea040d0b973adfb990ddb4dd55e0e
Nikhil Prajapati <nikhilprajapati3003@gmail.com>
2025-08-15 08:28:35 +0530

    add comment like & dislike functionality

--- src/modules/comments/ui/components/comment-item.tsx
+++ src/modules/comments/ui/components/comment-item.tsx
@@ -10,9 +10,16 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
-import { MessageSquareIcon, MoreVerticalIcon, Trash2Icon } from "lucide-react";
+import {
+  MessageSquareIcon,
+  MoreVerticalIcon,
+  ThumbsDownIcon,
+  ThumbsUpIcon,
+  Trash2Icon,
+} from "lucide-react";
 import { useAuth, useClerk } from "@clerk/nextjs";
 import { toast } from "sonner";
+import { cn } from "@/lib/utils";
 
 interface CommentItemsProps {
   comment: CommentsGetManyOutput["items"][number];
@@ -26,6 +33,29 @@ export const CommentItem = ({ comment }: CommentItemsProps) => {
     onSuccess: () => {
       utils.comments.getMany.invalidate({ videoId: comment.videoId });
       toast.success("Comment deleted");
+    },
+    onError: (error) => {
+      toast.error("Something went wrong");
+      if (error.data?.code === "UNAUTHORIZED") {
+        clerk.openSignIn();
+      }
+    },
+  });
+
+  const like = trpc.commentReactions.like.useMutation({
+    onSuccess: () => {
+      utils.comments.getMany.invalidate({ videoId: comment.videoId });
+    },
+    onError: (error) => {
+      toast.error("Something went wrong");
+      if (error.data?.code === "UNAUTHORIZED") {
+        clerk.openSignIn();
+      }
+    },
+  });
+  const dislike = trpc.commentReactions.dislike.useMutation({
+    onSuccess: () => {
+      utils.comments.getMany.invalidate({ videoId: comment.videoId });
     },
     onError: (error) => {
       toast.error("Something went wrong");
@@ -58,8 +88,43 @@ export const CommentItem = ({ comment }: CommentItemsProps) => {
               </span>
             </div>
           </Link>
-          <p>{comment.value}</p>
-          {/* TODO: Reactions */}
+          <p className="text-sm">{comment.value}</p>
+          <div className="flex items-center gap-2 mt-1">
+            <div className="flex items-center">
+              <Button
+                disabled={like.isPending}
+                variant="ghost"
+                size="icon"
+                className="size-8"
+                onClick={() => like.mutate({ commentId: comment.id })}
+              >
+                <ThumbsUpIcon
+                  className={cn(
+                    comment.viewerReaction === "like" && "fill-black"
+                  )}
+                />
+              </Button>
+              <span className="text-xs text-muted-foreground">
+                {comment.likeCount}
+              </span>
+              <Button
+                disabled={dislike.isPending}
+                variant="ghost"
+                size="icon"
+                className="size-8"
+                onClick={() => dislike.mutate({ commentId: comment.id })}
+              >
+                <ThumbsDownIcon
+                  className={cn(
+                    comment.viewerReaction === "dislike" && "fill-black"
+                  )}
+                />
+              </Button>
+              <span className="text-xs text-muted-foreground">
+                {comment.dislikeCount}
+              </span>
+            </div>
+          </div>
         </div>
         <DropdownMenu modal={false}>
           <DropdownMenuTrigger asChild>
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,22 +1,24 @@
 
-import { categoriesRouter } from "@/modules/categories/server/procedures";
 import { createTRPCRouter } from "../init";
-import { studioRouter } from "@/modules/studio/server/procedures";
 import { videosRouter } from "@/modules/videos/server/procedures";
-import { videoViewsRouter } from "@/modules/video-views/server/procedures";
-import { videoReactionsRouter } from "@/modules/video-reactions/server/procedures";
-import { subscriptionsRouter } from "@/modules/subscriptions/server/procedures";
+import { studioRouter } from "@/modules/studio/server/procedures";
 import { commentsRouter } from "@/modules/comments/server/procedures";
+import { categoriesRouter } from "@/modules/categories/server/procedures";
+import { videoViewsRouter } from "@/modules/video-views/server/procedures";
+import { subscriptionsRouter } from "@/modules/subscriptions/server/procedures";
+import { videoReactionsRouter } from "@/modules/video-reactions/server/procedures";
+import { commentReactionsRouter } from "@/modules/comment-reactions /server/procedures";
 
 
 export const appRouter = createTRPCRouter({
-  studio : studioRouter,
-  categories: categoriesRouter,
   videos: videosRouter,
+  studio : studioRouter,
   comments: commentsRouter,
   videoViews: videoViewsRouter,
+  categories: categoriesRouter,
   subscriptions: subscriptionsRouter,
   videoReactions: videoReactionsRouter,
+  commentReactions: commentReactionsRouter, 
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
